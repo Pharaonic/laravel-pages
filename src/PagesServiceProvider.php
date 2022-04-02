c4566c34116755187f1b842222c00ce241348492
Moamen Eltouny <16774083+MoamenEltouny@users.noreply.github.com>
2022-04-02 12:14:33 +0200

    add migrations publishing

--- src/PagesServiceProvider.php
+++ src/PagesServiceProvider.php
@@ -13,6 +13,11 @@ class PagesServiceProvider extends ServiceProvider
 
     public function register()
     {
-        // 
+        // Publishes
+        $this->publishes([
+            __DIR__ . '/Migrations/2021_02_01_000006_create_pages_table.php'                   => database_path('migrations/2021_02_01_000006_create_pages_table.php'),
+            __DIR__ . '/Migrations/2021_02_01_000007_create_page_translations_table.php'       => database_path('migrations/2021_02_01_000007_create_page_translations_table.php'),
+            __DIR__ . '/Migrations/2021_02_01_000008_create_model_has_pages_table.php'         => database_path('migrations/2021_02_01_000008_create_model_has_pages_table.php'),
+        ], ['pharaonic', 'laravel-pages']);
     }
 }
